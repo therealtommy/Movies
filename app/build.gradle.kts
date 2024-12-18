409c96d81420c2c4121addc57edc0cbaaea79f24
tommyallen <thomasclansi228@gmail.com>
2024-12-19 00:35:03 +0700

    Menu initialization

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,20 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-
     kotlin("kapt")
 }
 
 android {
     namespace = "com.example.movies"
-    compileSdk = 34
+    compileSdk = 35
+
+    buildFeatures {
+        viewBinding = true
+    }
 
     defaultConfig {
         applicationId = "com.example.movies"
-        minSdk = 24
+        minSdk = 29
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
