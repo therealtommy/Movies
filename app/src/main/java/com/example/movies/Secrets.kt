542ca0a01e5c260e04d8944f8432212e62a84efd
tommyallen <thomasclansi228@gmail.com>
2024-12-18 22:20:54 +0700

    Add API key for IMDB in project

---
+++ app/src/main/java/com/example/movies/Secrets.kt
@@ -0,0 +1,21 @@
+package com.example.movies
+
+import java.io.FileInputStream
+import java.util.Properties
+
+object Secrets {
+    private const val PROPERTIES_FILE = "secrets.properties"
+    private val properties: Properties = Properties()
+
+    init {
+        try {
+            properties.load(FileInputStream(PROPERTIES_FILE))
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    fun getApiKey(): String? {
+        return properties.getProperty("OMDB_API_KEY")
+    }
+}
